0b336844c9de15a729f95eff09f9b2e980ce8742
Vincent Loupmon <6957532+Vloupmon@users.noreply.github.com>
2021-07-30 12:01:25 +0200

    update LINQ

---
+++ LINQ/Manipulation d'objets/Manipulation d'objets/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions {
+
+    public static class StringExtension {
+
+        public static int WordCount(this string str) {
+            int i = 0;
+            foreach (string word in str.Split(' ')) {
+                i++;
+            }
+            return (i);
+        }
+    }
+}
---
+++ LINQ/Manipulation d'objets/Manipulation d'objets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Extensions;
+
+namespace Manipulation_d_objets {
+
+    internal class Program {
+        private Func<string, string, int> ConcatSum = (x, y) => x.Length + y.Length;
+
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!".WordCount());
+        }
+    }
+}
