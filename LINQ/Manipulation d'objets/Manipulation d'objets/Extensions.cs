829bf1660ee42b4812741f480866f6c1e4ef76b7
Vloupmon <6957532+Vloupmon@users.noreply.github.com>
2021-08-22 01:48:38 +0200

    Documents HTML CSS

--- LINQ/Manipulation d'objets/Manipulation d'objets/Extensions.cs
+++ LINQ/Manipulation d'objets/Manipulation d'objets/Extensions.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace Extensions {
 
+    public static class LINQExtension {
+        public static double Median(this IEnumerable<double>? source) {
+            if (!(source?.Any() ?? false)) {
+                throw new InvalidOperationException("Ensemble NULL ou vide.");
+            }
+
+            source = (from number in source
+                      orderby number
+                      select number).ToList<double>();
+            int index = source.Count() / 2;
+
+            if (source.Count() % 2 == 0) {
+                return (source.ElementAt(index) + source.ElementAt(index) - 1) / 2;
+            } else {
+                return source.ElementAt(index);
+            }
+        }
+    }
+
     public static class StringExtension {
 
         public static int WordCount(this string str) {
@@ -13,7 +32,7 @@ namespace Extensions {
             foreach (string word in str.Split(' ')) {
                 i++;
             }
-            return (i);
+            return i;
         }
     }
 }
--- LINQ/Manipulation d'objets/Manipulation d'objets/Program.cs
+++ LINQ/Manipulation d'objets/Manipulation d'objets/Program.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Linq;
-using Extensions;
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
 
 namespace Manipulation_d_objets {
 
     internal class Program {
-        private Func<string, string, int> ConcatSum = (x, y) => x.Length + y.Length;
 
         private static void Main(string[] args) {
-            Console.WriteLine("Hello World!".WordCount());
+            Func<string, string, int> ConcatSum = (x, y) => x.Length + y.Length;
+
+            Console.WriteLine("Wordcount of \"Hello World!\" : " + "Hello World!".WordCount());
+            Console.WriteLine("\nConcatSum of \"Hello\" + \"World!\" : " + ConcatSum("Hello", "World!").ToString());
+            Console.WriteLine("\nMedian of \"1, 2, 3, 4, 5, 6, 7, 8, 9\" : " + 
+                new double[] {1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 }.Median().ToString());
+            Console.WriteLine("\nMedian of \"1, 2, 3, 4, 5, 6, 7, 8, 9, 10\" : " + 
+                new double[] {1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9, 10 }.Median().ToString());
+            Console.WriteLine("\nMedian of \"1\" : " + 
+                new double[] {1}.Median().ToString());
+            Console.WriteLine("\nMedian of \"\" : ");
+            try {
+                Console.WriteLine(Array.Empty<double>().Median().ToString());
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
