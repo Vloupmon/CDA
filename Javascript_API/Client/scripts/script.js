94e307837be8b79433640f40e311bdfd6f2f45f5
Vincent Loupmon <6957532+Vloupmon@users.noreply.github.com>
2021-09-13 01:46:32 +0200

    API update

--- Javascript_API/Client/scripts/script.js
+++ Javascript_API/Client/scripts/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const placesList = new Array();
+var imgData;
 
 $(document).ready(function () {
   $("#zipCode").keyup(function (e) {
@@ -10,15 +11,21 @@ $(document).ready(function () {
         .val()
         .match(/[0-9]{5}$/g)
     ) {
-      zipCodeFetch($(this).val());
+      getZipCodeData($(this).val());
     }
   });
 
-  $("#city").change(function (e) {
-    console.log(e.target.value);
+  $("#fileInput").on("change", function () {});
+
+  $("#imgForm").change(function () {
+    var fileReader = new FileReader();
+    fileReader.onload = function () {
+      imgData = fileReader.result;
+    };
+    fileReader.readAsDataURL($("#btnFiles").prop("files")[0]);
   });
 
-  $("form").validate({
+  $("#userForm").validate({
     rules: {
       name: "required",
       userName: "required",
@@ -35,29 +42,87 @@ $(document).ready(function () {
       phone: "required",
     },
     submitHandler: function () {
-      setData();
+      postUser();
     },
   });
 });
 
-function zipCodeFetch(zipCode) {
-  var hdrs = new Headers();
-  var initialisation = {
+async function userSubmitHandler() {
+  const userId = await postUser();
+  console.log("userId: " + userId);
+  //postImg(userId);
+}
+
+function getZipCodeData(zipCode) {
+  var headers = new Headers();
+  var init = {
     method: "GET",
-    headers: hdrs,
+    headers: headers,
     mode: "cors",
     cache: "default",
   };
-  var requete = new Request(
-    "https://api.zippopotam.us/fr/" + zipCode,
-    initialisation
-  );
-  fetch(requete)
+  var request = new Request("https://api.zippopotam.us/fr/" + zipCode, init);
+  fetch(request)
     .then(async function (response) {
       if (response.ok) {
         const json = await response.json();
         setPlacesArray(json.places);
         setPlacesInput();
+      } else {
+        console.log("Bad answer from the server");
+      }
+    })
+    .catch(function (error) {
+      console.log("There's a problem with the fetch request: " + error.message);
+    });
+}
+
+async function postUser() {
+  var jsonData = formDataToJson();
+  var init = {
+    method: "POST",
+    body: jsonData,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  var request = new Request("https://localhost:44331/api/users", init);
+
+  fetch(request)
+    .then(async function (response) {
+      if (response.ok) {
+        const json = await response.json();
+        if (json.userId != null) {
+          console.log(response);
+          postImg(json.userId);
+        }
+      } else {
+        console.log("Bad answer from the server");
+        return false;
+      }
+    })
+    .catch(function (error) {
+      console.log("There's a problem with the fetch request: " + error.message);
+      return false;
+    });
+}
+
+function postImg(userId) {
+  const headers = new Headers();
+  const init = {
+    method: "POST",
+    body: imgData,
+    headers: headers,
+  };
+  const request = new Request(
+    "https://localhost:44331/api/UploadImages/" + userId,
+    init
+  );
+
+  fetch(request)
+    .then(async function (response) {
+      if (response.ok) {
+        console.log(response.body);
       } else {
         console.log("Bad answer from the server");
       }
@@ -77,7 +142,6 @@ function setPlacesArray(jsonPlaces) {
       longitude: place["longitude"],
     });
   }
-  console.log(placesList);
   setPlacesInput();
 }
 
@@ -87,17 +151,27 @@ function setPlacesInput() {
   });
 }
 
-function setData() {
-  /*  var jsonData = JSON.stringify({
-    name: $("#name").val(),
-    placesList
-  });*/
-  var jsonData = $("form").serializeJSON();
-  var geo = {
-    latitude: placesList[$("#city").val()]["latitude"],
-    longitude: placesList[$("#city").val()]["longitude"],
-  };
-  console.log(geo);
-  jsonData.address.geo = geo;
-  console.log(jsonData);
+function formDataToJson() {
+  var jsonData = JSON.stringify(
+    {
+      name: $("#name").val(),
+      userName: $("#userName").val(),
+      email: $("#email").val(),
+      address: {
+        street: $("#street").val(),
+        suite: $("#suite").val(),
+        city: $("#city option:selected").text(),
+        zipcode: $("#zipCode").val(),
+        geo: {
+          latitude: placesList[$("#city").val()]["latitude"],
+          longitude: placesList[$("#city").val()]["longitude"],
+        },
+      },
+      phone: $("#phone").val(),
+      birthDay: $("#birthDay").val(),
+    },
+    null,
+    "\t"
+  );
+  return jsonData;
 }
