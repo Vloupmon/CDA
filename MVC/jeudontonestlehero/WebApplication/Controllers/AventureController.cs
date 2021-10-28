b257946fed366a5fdadaf34af0524ec5798ceeab
Vloupmon <6957532+Vloupmon@users.noreply.github.com>
2021-10-28 18:05:31 +0200

    Update MVC

---
+++ MVC/jeudontonestlehero/WebApplication/Controllers/AventureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jeudontonestleheros.Web.UI.Controllers {
+
+    public class AventureController : Controller {
+
+        public IActionResult Index() {
+            ViewBag.collection = new int[] { 1, 2, 3, 4, 5 };
+
+            return View();
+        }
+    }
+}
